e85084ac4e2a2d5ea439f0a981a8cf00a246f177
shilsmarty <shiladitya.srivastava@gmail.com>
2017-08-02 00:50:42 -0700

    Adding test Solution.

---
+++ LeetCodeSolution/SolutionTests/ArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolutionTests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
